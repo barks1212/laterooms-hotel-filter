1f7e45fdae33fdb87d21c8f10d55c23dd11b7e06
barks1212 <willbarkerwork@gmail.com>
2018-03-05 09:35:14 +0000

    basic header layout, hotel data added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Header = () => (
-  <section>test</section>
+  <section className="hero is-mobile is-info is-bold is-small">
+    <section className="hero-head">
+      <section className="container">
+        <span className="logo"><img src="https://p3.zdassets.com/hc/settings_assets/1284070/200291205/Ot3O9UQvJGka8SfWWLVjRQ-LateRooms_Logo_H_RGB.png" alt="logo" id="logo"/></span>
+      </section>
+      <section className="hero-body">
+        <section className="container is-mobile has-text-centered" id="headerBox">
+          <h2 className="title is-size-6" id="searchResults">Search Results</h2>
+        </section>
+      </section>
+    </section>
+
+  </section>
 );
 
 export default Header;
