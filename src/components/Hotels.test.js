30dbc388820cb10593453ac453b082cc5ab02a1d
barks1212 <willbarkerwork@gmail.com>
2018-03-05 11:09:44 +0000

    tests written for sort functionality

--- src/components/Hotels.test.js
+++ src/components/Hotels.test.js
@@ -3,11 +3,27 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import Hotels from './Hotels';
 
 describe('<Hotels>', () => {
   it('renders as expected without crashing and matches snapshot', () => {
     const tree = renderer.create(<Hotels />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('can sort hotels based on stars in ascending order', () => {
+    const wrapper = mount(<Hotels />);
+    const hotels = wrapper.state().hotelList;
+    wrapper.find('button').at(1).simulate('click');
+    expect(hotels[0].StarRating).toEqual(2);
+    expect(hotels[hotels.length - 1].StarRating).toEqual(5);
+  });
+
+  it('can sort hotels based on stars in descending order', () => {
+    const wrapper = mount(<Hotels />);
+    const hotels = wrapper.state().hotelList;
+    wrapper.find('button').at(0).simulate('click');
+    expect(hotels[0].StarRating).toEqual(5);
+    expect(hotels[hotels.length - 1].StarRating).toEqual(2);
   });
 });
