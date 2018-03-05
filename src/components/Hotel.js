4631cac2f9cb6ce3b1e5ad78221cbb70b0a716f1
barks1212 <willbarkerwork@gmail.com>
2018-03-05 10:46:57 +0000

    hotels rendering with images, star ratings and facilities

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -2,9 +2,36 @@ import React from 'react';
 import PT from 'prop-types';
 
 const Hotel = (props) => {
+  const arr = new Array(props.hotel.StarRating);
+  const stars = arr.fill(<i className="fa fa-star" id="stars" aria-hidden="true"></i>);
+
   return (
-    <section>working</section>
+    <section className="hotels">
+      <section className="card is-mobile" id="hotelCard">
+        <figure className="image is-128x128">
+          <img src={props.hotel.Image} alt="" id="hotelPic" />
+        </figure>
+        <section className="nameAndRating">
+          <h2 className="subtitle has-text-weight-bold is-size-7 is-marginless">{props.hotel.Name}</h2>
+          <span className="starRating">{stars}</span>
+        </section>
+      </section>
+      <section className="hotelContent">
+        <section className="container is-mobile" id="facilities">
+          {props.hotel.Facilities.map((facility, i) => {
+            return (
+              <p className="facility is-size-6 is-mobile" key={i}>{`${facility[0].toUpperCase()}${facility.substring(1)}`}</p>
+            );
+          })}
+        </section>
+      </section>
+    </section>
   );
+}
+
+Hotel.propTypes = {
+  hotel: PT.object.isRequired,
+  key: PT.number
 };
 
 export default Hotel;
