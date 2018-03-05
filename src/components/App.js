3c2e04ae01ac3d4536d4468159e113e114cd3812
barks1212 <willbarkerwork@gmail.com>
2018-03-05 09:28:03 +0000

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../main.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <section className="main">
+        <p>working</p>
+      </section>     
+    );
+  }
+}
+
+export default App;
