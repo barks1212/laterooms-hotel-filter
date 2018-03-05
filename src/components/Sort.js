a3e2d19e7d95bc4a8c79116e2be1d2008515d854
barks1212 <willbarkerwork@gmail.com>
2018-03-05 10:59:55 +0000

    buttons added, no functionality

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,7 +2,12 @@ import React from 'react';
 import PT from 'prop-types';
 
 const Sort = (props) => (
-  <section>working</section>
+  <section className="sort is-paddingless is-marginless">
+    <span className="is-paddingless has-text-weight-bold"> Sort by </span>
+    <button className="button is-small is-paddingless is-marginless is-text" id="sortButton">Descending</button>
+    |
+    <button className="button is-small is-paddingless is-marginless is-text" id="sortButton" >Ascending</button>
+  </section>
 );
 
 Sort.propTypes = {
